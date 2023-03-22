788d0ee0d2425c8ae31ec3bbd3241154ad204ccb
Hassan-Attia1 <hassan.attia1997@gmail.com>
2023-03-22 17:29:05 +0200

    A .c file contains program to print a sentence

---
+++ Unit_2_C_Programming/1_C_Basics_Assignments/Assignment_1/Ex_1_printing_a_sentence.c
@@ -0,0 +1,18 @@
+/*
+ * Ex_1_printing_a_sentence.c
+ *
+ *  Created on: 22 Mar 2023
+ *      Author: hassan attia
+ */
+
+
+#include<stdio.h>
+
+
+int main(){
+
+	printf("C Programming");
+
+
+	return 0;
+}
